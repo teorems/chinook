182620e7bf3d194a35eed4b0b7f664863fd928f5
Emanuele Messori <teorems@gmail.com>
2021-09-08 22:23:45 +0200

    Initial commit

---
+++ functions.R
@@ -0,0 +1,16 @@
+query <- ""
+
+run_query <- function(q) {
+  conn = dbConnect(SQLite(),'chinook.db')
+  result <- dbGetQuery(conn, q)
+  dbDisconnect(conn)
+  return(result)
+}
+
+show_tables <- function() {
+  run_query('SELECT
+    name,
+    type
+FROM sqlite_master
+WHERE type IN ("table","view");')
+}
